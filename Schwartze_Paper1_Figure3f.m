2e1fa1bc1538291f4518dc0ea8da886f6cf1f698
Adam G. Rouse <56125401+arouseKUMC@users.noreply.github.com>
2023-07-05 16:23:17 -0500

    Updated Figure 3ef

--- Schwartze_Paper1_Figure3f.m
+++ Schwartze_Paper1_Figure3f.m
@@ -25,8 +25,9 @@ All_best_RMSE_cor = cat(2,best_RMSE_cor{:});
 init_color = [0,0,0.7];
 cor_color = [0.7,0,0];
 
-correlation_matrix = corrcoef(All_best_RMSE_init(All_signif_units), All_best_RMSE_cor(All_signif_units));
+[correlation_matrix,~,correlation_matrix_lower, correlation_matrix_upper] =  corrcoef(All_best_RMSE_init(All_signif_units), All_best_RMSE_cor(All_signif_units));
 correlation = correlation_matrix(1, 2);
+disp(['Corr: ' num2str(correlation) ', (' num2str(correlation_matrix_lower(1,2)) '-' num2str(correlation_matrix_upper(1,2)) ') 95%CI'])
 
 
 figure
@@ -40,9 +41,9 @@ ylabel('Corrective Model RMSE', 'Color', cor_color)
 set(gca, 'FontSize', 14)
 set(gcf,'Position',[0 0 600 400]);
 hold on;
-fit = polyfit(All_best_RMSE_init(All_signif_units), All_best_RMSE_cor(All_signif_units), 1);
-y_fit = polyval(fit, All_best_RMSE_init(All_signif_units));
-plot(All_best_RMSE_init(All_signif_units), y_fit, 'color','cyan');
+% fit = polyfit(All_best_RMSE_init(All_signif_units), All_best_RMSE_cor(All_signif_units), 1);
+% y_fit = polyval(fit, All_best_RMSE_init(All_signif_units));
+% plot(All_best_RMSE_init(All_signif_units), y_fit, 'color','cyan');
 % print(gcf, ['./Schwartze_Paper1/figure_graphics/' monkey 'figure5_RMSE_init-cor_p'], '-dpng')
 
 %paired t-test
